b5496aeb4cc1ce7c8813b9a2aeb91b0fa8c63129
IsmailBourbie <bourbieismail@gmail.com>
2020-04-01 23:42:20 +0100

    finsihed main page design

--- index.php
+++ index.php
@@ -33,22 +33,36 @@
     <!-- Start with Main -->
     <div class="main">
         <div class="container">
-            <header class="main-title">
+            <div class="main-header">
                 <h1>
                     <i class="fa fa-1x fa-info-circle"></i>
                     معلومــات مهمة
                 </h1>
-            </header>
-            <div id="section1" class="section">
+            </div>
+            <div class="section">
                 <div class="row">
-                    <div class="col-lg-6" style="text-align: center">
+                    <div class="col-md-6 col-lg-push-6">
+                        <p class="lead">هل تعاني ( انت أو أي شخص تعرفه) من الحمى أو السعال؟ هل أنت قلق من أنك قد تكون مصابًا بعدوى فيروس كورونا؟ هل تحتاج الى نصيحة طبية؟ سنساعدك. يساعدك استبيان التقييم الذاتي هذا على تلخيص العناصر الرئيسية التي تستلزم زيارة الطبيب أو الإتصال بالمصالح المعنية .</p>
+                    </div>
+                    <div class="col-md-6 col-lg-pull-6" style="text-align: center">
                         <img src="http://covid-quest.com/images/3.fievre.png">
                         <img src="http://covid-quest.com/images/2.toux.png">
                     </div>
-                    <div class="col-lg-6">
-                        <p class="lead">هل تعاني ( انت أو أي شخص تعرفه) من الحمى أو السعال؟ هل أنت قلق من أنك قد تكون مصابًا بعدوى فيروس كورونا؟ هل تحتاج الى نصيحة طبية؟ سنساعدك. يساعدك استبيان التقييم الذاتي هذا على تلخيص العناصر الرئيسية التي تستلزم زيارة الطبيب أو الإتصال بالمصالح المعنية .</p>
-                    </div>
                 </div>
+            </div>
+            <div class="section">
+                <p class="lead">تسمح أجوبتك على الاستبيان بالحصول على نموذج يمكن إرساله إلى طبيبك أو إلى المصالح المختصة لطلب موعد.
+                    يقدم لك هذا الاستبيان أيضًا معلومات خاصة بحالتك حيث يمكن إكمال القيام بتقييم ذاتي بواسطة الشخص المريض نفسه أو مقدم الرعاية( خاصة بالنسبة لكبار السن أو الأشخاص المتعبين جدًا ) .
+                    لا يهدف هذا الاستبيان إلى استبدال المشورة الطبية ، فهو مصمم لتسهيل الحوار مع الطبيب فقط .
+                    ملاحظة: هذا الاستبيان غير مناسب للأطفال.</p>
+            </div>
+
+            <div class="section">
+                <p class="lead">تم تصميم الإستبيان من قبل أطباء بالشراكة مع مهندسي إعلام .</p>
+            </div>
+
+            <div class="main-footer">
+                <button class="btn btn-success">أبدأ الأسئلة</button>
             </div>
         </div>
     </div>
