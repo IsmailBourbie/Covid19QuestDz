e04c067b0f4b0bfe2a900da52756bee25c3aba18
IsmailBourbie <bourbieismail@gmail.com>
2020-04-01 23:22:45 +0100

    added first section

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/main.css">
     <title>Document</title>
 </head>
@@ -28,6 +30,28 @@
         </div>
     </nav>
 
+    <!-- Start with Main -->
+    <div class="main">
+        <div class="container">
+            <header class="main-title">
+                <h1>
+                    <i class="fa fa-1x fa-info-circle"></i>
+                    معلومــات مهمة
+                </h1>
+            </header>
+            <div id="section1" class="section">
+                <div class="row">
+                    <div class="col-lg-6" style="text-align: center">
+                        <img src="http://covid-quest.com/images/3.fievre.png">
+                        <img src="http://covid-quest.com/images/2.toux.png">
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="lead">هل تعاني ( انت أو أي شخص تعرفه) من الحمى أو السعال؟ هل أنت قلق من أنك قد تكون مصابًا بعدوى فيروس كورونا؟ هل تحتاج الى نصيحة طبية؟ سنساعدك. يساعدك استبيان التقييم الذاتي هذا على تلخيص العناصر الرئيسية التي تستلزم زيارة الطبيب أو الإتصال بالمصالح المعنية .</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="js/jquery-1.12.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
